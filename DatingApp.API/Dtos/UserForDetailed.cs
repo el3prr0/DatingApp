2da877a71a7c4c24b6a705537934a17147292aef
Omar Javier Aguirre Segura (Contractor) <omarjavier.aguirresegura@gilead.com>
2019-04-01 15:52:17 -0700

    End section 8

--- DatingApp.API/Dtos/UserForDetailed.cs
+++ DatingApp.API/Dtos/UserForDetailed.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
 
-namespace DatingApp.API.Models
-{
-    public class User
-    {
 
+namespace DatingApp.API.Dtos
+{
+    public class UserForDetailed
+    {
         public int Id { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+
 
         public string Gender{get;set;}
-        public DateTime DateOfBirth{get;set;}
+        public int Age{get;set;}
         public string KnownAs{get;set;}
         public DateTime Create {get;set;}
 
@@ -22,6 +21,7 @@ namespace DatingApp.API.Models
         public string Interests {get;set;}
         public string City {get;set;}
         public string Country{get;set;}
-        public ICollection<Photo> Photos {get;set;}
+        public string PhotoUrl{get;set;}
+        public ICollection<PhotosForDetailed> Photos {get;set;}
     }
 }
